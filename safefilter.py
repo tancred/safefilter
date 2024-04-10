74ec89648e3713564dece452d06558477c163893
Malte Tancred <malte@tancred.com>
2024-04-10 23:21:31 +0200

    Allow filterprog args. And bump to python3.
    Long overdue, this has been sitting in my private bin dir
    since... 2016! Well, the python3 only since late 2022. :-)

--- safefilter.py
+++ safefilter.py
@@ -1,41 +1,44 @@
-#!/usr/bin/env python
-import optparse
+#!/usr/bin/env python3
 import os
 import subprocess
 import sys
 import uuid
 
-parser = optparse.OptionParser(usage="Usage: %prog filterprog file")
-
 def die_usage():
-	parser.print_help()
+	print("Usage: {prg} filterprog [arg ... -- ] file".format(prg=sys.argv[0]), file=sys.stderr)
 	sys.exit(1)
 
-def do_filter(progname,filename):
-	# first run the equivalent of 'progname < filename > tmp'
+def do_filter(prog,filename):
+	# first run the equivalent of 'prog < filename > tmp'
 	fin = open(filename, "rb")
-	
+
 	# make sure the tmp file is created on the same file system as the original
 	d,f = os.path.split(filename)
 	tmpname = os.path.join(d, "%s.%s.tmp" % (f, uuid.uuid4()))
 	fout = open(tmpname, "wb")
-	
-	p = subprocess.Popen([progname], stdin=fin, stdout=fout)
+
+	p = subprocess.Popen(prog, stdin=fin, stdout=fout)
 	p.communicate()
 	if p.returncode != 0: sys.exit(1)
 	fin.close()
 	fout.close()
-	
+
 	# Now move the filtered file in place.
 	# Note: on win, atomic rename apparently isn't possible;
 	# because dst already exists, this code will raise.
 	os.rename(tmpname, filename)
 
 def do():
-	(options,args) = parser.parse_args()
+	args = sys.argv[1:]
 	if len(args) < 2: die_usage()
-	opt_filterprog = args[0]
-	opt_file = args[1]
+	if len(args) == 2:
+		opt_filterprog = [args[0]]
+		opt_file = args[1]
+	else:
+		if args[-2] != '--': die_usage()
+		opt_filterprog = args[:-2]
+		opt_file = args[-1]
+
 	do_filter(opt_filterprog, opt_file)
 
 do()
